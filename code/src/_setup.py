57aec8519415c5b1cf2e290d7effbfc0a0b0d733
HoChan Cheon <hcheon90@users.noreply.github.com>
2025-04-07 09:22:24 +0900

    Create src
    Add source scripts:
    1. Data IO, management
    2. Functions for statistics
    3. Cyton2, Cython compiler

---
+++ code/src/_setup.py
@@ -0,0 +1,17 @@
+import numpy
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        name="model",
+        sources=["model.pyx"],
+        # language='c++',
+        extra_compile_args=['-O3'],
+        include_dirs=[numpy.get_include()]
+    )
+]
+
+setup(
+    ext_modules=cythonize(extensions)
+)
