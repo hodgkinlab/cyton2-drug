240e43734c23b8b1bcd7568cc7d578b369b700cd
HoChan Cheon <hcheon90@users.noreply.github.com>
2025-04-14 12:31:42 +0900

    Update utils.py
    Remove unused functions

--- code/src/utils.py
+++ code/src/utils.py
@@ -3,44 +3,9 @@ import scipy.stats as sps
 
 def norm_pdf(x, mu, sig):
 	return sps.norm.pdf(x, mu, sig)
+
 def norm_cdf(x, mu, sig):
 	return sps.norm.cdf(x, mu, sig)
-
-def truncnorm_pdf(x, mu, sig):
-	return sps.truncnorm.pdf(x, a=(0-mu)/sig, b=np.inf, loc=mu, scale=sig)
-def truncnorm_cdf(x, mu, sig):
-	return sps.truncnorm.cdf(x, a=(0-mu)/sig, b=np.inf, loc=mu, scale=sig)
-
-def lognorm_pdf(x, m, s):
-	return sps.lognorm.pdf(x, s, scale=m)
-def lognorm_cdf(x, m, s):
-	return sps.lognorm.cdf(x, s, scale=m)
-
-def lognorm_statistics(m, s, return_std=False):
-	"""
-	Calculates statistics of the mean and variance of Lognormal distribution.
-
-	:param m: (float) the median
-	:param s: (float) the standard deviation
-	:param std: (float) return standard deviation if True
-	"""
-	mean = np.exp(np.log(m) + s**2/2)
-	var = np.exp(2*np.log(m) + 2*s**2) - np.exp(2*np.log(m) + s**2)
-	if return_std:
-		return mean, np.sqrt(var)
-	return mean, var
-
-def ecdf(x):
-	"""
-	Calculate empirical cumulative distribution.
-
-	:param x: (list) data
-	:return: (tuple) x and y
-	"""
-	n = len(x)
-	xs = np.sort(x)
-	ys = np.arange(1, n+1)/float(n)
-	return xs, ys
 
 def conf_iterval(l, rgs):
 	alpha = (100. - rgs)/2.
